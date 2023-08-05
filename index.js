569ce8509ee5e44342b2d411e748186aad5caa30
MAXIFUN35 <109604964+MAXIFUN35@users.noreply.github.com>
2023-08-05 07:56:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 function onScroll() {
   var scrollPosition = window.scrollY;
-
+  const viewportHeight = window.innerHeight * 0.01;
+  const alturaEnVh = 10; // Puedes cambiar este valor a cualquier otra altura deseada
+  const alturaEnPixels = viewportHeight * alturaEnVh;
   var triggerPosition = 280; 
   var header = document.getElementById('titulo');
   const alto = header.offsetHeight;
   const alturaViewport = window.innerHeight;
-  var degrade = document.getElementById('titulo__nav') 
+  var degrade = document.getElementById('titulo__nav'); 
 
-  if (alto < alturaViewport ){
+  if (alto < alturaEnPixels ){
     if (scrollPosition >= triggerPosition) {
         header.style.backgroundColor = 'white';
         header.style.background = 'white';
@@ -19,12 +21,13 @@ function onScroll() {
     } else {
         header.style.backgroundColor = 'transparent';
         header.style.animation = '.3s';
-        degrade.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, .2),rgba(0, 0, 0, 0))';
+        degrade.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1),  rgba(0, 0, 0, 0.0))';
     }
   }
 
 }
 window.addEventListener('scroll', onScroll);
+// contacto
 function scrollToSection() {
 
   var destinationElement = document.querySelector('#contacto1');
@@ -41,43 +44,48 @@ function isMobileDevice() {
   const checkbox = document.getElementById('checkmenu');
   const desplegable = document.getElementById('titulo');
   const imagen = document.getElementById('barramenu');
-  const menu = document.getElementById('menu')
+  const menu = document.getElementById('menu');
   let isDesplegableOpen = false;
-  var degrade = document.getElementById('titulo__nav')
+  var degrade = document.getElementById('titulo__nav');
   
 
   function cerrarDesplegable() {
     desplegable.style.height = '3em';
-    menu.style.display = 'none';
+    menu.style.display = 'block';
     var scrollPosition = window.scrollY;
-
     var triggerPosition = 180; 
     var header = document.getElementById('titulo');
+    const alto = header.offsetHeight;
+    const viewportHeight = window.innerHeight * 0.01;
+    const alturaEnVh = 10; // Puedes cambiar este valor a cualquier otra altura deseada
+    const alturaEnPixels = viewportHeight * alturaEnVh;
 
       if (scrollPosition >= triggerPosition) {
           header.style.backgroundColor = 'white';
           desplegable.style.height = '3em';
-          degrade.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, 0),rgba(0, 0, 0, 0))';
-
+          degrade.style.background = 'linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0))';
       } else {
           header.style.backgroundColor = 'transparent';
           header.style.animation = '.3s';
           desplegable.style.height = '3em';
-          degrade.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, .2),rgba(0, 0, 0, 0))';
-          
-          
+          degrade.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, .1), rgba(0, 0, 0, .01))';
       }
     isDesplegableOpen = false;
+        
+    if(alto < alturaEnPixels){
+      menu.style.display = 'none';
+    }
   }
   var header = document.getElementById('titulo');
   checkbox.addEventListener('change', function() {
     if(isMobileDevice){
      if (checkbox.checked) {
-      desplegable.style.height = '100vh';
+      desplegable.style.height = '40vh';
       desplegable.style.backgroundColor = 'white';
+      desplegable.style.transition = 'ease .3s';
       menu.style.display = 'block';
       isDesplegableOpen = true;
-      degrade.style.background = 'linear-gradient(to bottom, rgba(255, 255, 255, 1),rgba(255, 255, 255, 1))';
+      degrade.style.background = 'linear-gradient(to bottom, rgba(255, 255, 255, 0),rgba(255, 255, 255, 0))';
      } else {
       cerrarDesplegable();
      }
